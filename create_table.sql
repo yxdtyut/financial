2aeb5e090c4da5bfb31cf346cfe0817a288d7cab
yangxudong <411867545@qq.com>
2018-06-06 09:41:43 +0800

    理财系统

--- create_table.sql
+++ create_table.sql
@@ -24,7 +24,7 @@ CREATE TABLE `order_t` (
   `order_status` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '状态,INIT:初始化,PROCESS:处理中,SUCCESS:成功,FAIL:失败	',
   `outer_order_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '外部订单编号',
   `amount` decimal(15,3) NOT NULL COMMENT '金额',
-  `memo` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '订单编号',
+  `memo` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
   `create_at` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建时间',
   `update_at` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`order_id`)
